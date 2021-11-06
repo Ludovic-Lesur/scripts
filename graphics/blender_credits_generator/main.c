a21c0b7651e094b13059e2acb3e62bbcda4d11b0
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2021-11-06 16:29:43 +0100

    Add header to credits lines to manage spacing.

--- graphics/blender_credits_generator/main.c
+++ graphics/blender_credits_generator/main.c
@@ -21,10 +21,17 @@
 
 /*** MAIN macros ***/
 
+#define CREDIT_LINE_LENGTH						33
+
+#define BLENDER_HEADER							L"* "
+#define BLENDER_HEADER_LENGTH					2
+
 #define BLENDER_TEXT_OBJECT_MAX_LENGTH			512
-#define BLENDER_CREDIT_LINE_LENGTH				24
+#define BLENDER_CREDIT_LINE_LENGTH				(CREDIT_LINE_LENGTH + BLENDER_HEADER_LENGTH)
+
 #define BLENDER_NUMBER_OF_LINE_PER_TEXT_OBJECT	(BLENDER_TEXT_OBJECT_MAX_LENGTH / BLENDER_CREDIT_LINE_LENGTH)
-#define LINE_BUFFER_LENGTH						32
+
+#define LINE_BUFFER_LENGTH						64
 //#define DEBUG
 
 /*** MAIN functions ***/
@@ -39,22 +46,29 @@ int main(void) {
 	// Local variables.
 	wchar_t credit_line[LINE_BUFFER_LENGTH] = {0x00};
 	unsigned int credit_line_idx = 0;
-	unsigned int credit_line_char_idx = 0;
+	unsigned int char_idx = 0;
 	unsigned int credit_line_length = 0;
 	wchar_t blender_line[BLENDER_TEXT_OBJECT_MAX_LENGTH] = {0x00};
 	unsigned int blender_subline_idx = 0;
+	unsigned int blender_line_idx = 0;
+	wchar_t blender_header[BLENDER_HEADER_LENGTH] = BLENDER_HEADER;
 	// Open files.
 	FILE* credits_in = fopen("./credits.txt", "r");
 	FILE* credits_out = fopen("./blender_lines.txt", "w+");
 	// Check file pointers.
 	if ((credits_in == NULL) || (credits_out == NULL)) {
 		wprintf(L"Open files : error.\n");
+		goto errors;
 	}
 	else {
 		wprintf(L"Open files : success.\n");
 	}
+	// Add header.
+	for (char_idx=0 ; char_idx<BLENDER_HEADER_LENGTH ; char_idx++) {
+		credit_line[char_idx] = blender_header[char_idx];
+	}
 	// Read input file.
-	while (fgetws(credit_line, LINE_BUFFER_LENGTH, credits_in) != NULL) {
+	while (fgetws(&(credit_line[BLENDER_HEADER_LENGTH]), (LINE_BUFFER_LENGTH - 2), credits_in) != NULL) {
 		// Get credit line.
 		credit_line_idx++;
 		// Check line length
@@ -70,33 +84,46 @@ int main(void) {
 		wprintf(L"length = %d\n\n", credit_line_length);
 #endif
 		if (credit_line_length > (BLENDER_CREDIT_LINE_LENGTH - 1)) {
-			wprintf(L"Error at line %d : length overflow\n", credit_line_idx);
-			//goto errors;
+			wprintf(L"Error at line %d : length overflow (%d characters read, %d maximum)\n", credit_line_idx, credit_line_length, BLENDER_CREDIT_LINE_LENGTH);
+			goto errors;
 		}
 		// Replace cariage return and pad with spaces.
-		for (credit_line_char_idx=0 ; credit_line_char_idx<LINE_BUFFER_LENGTH ; credit_line_char_idx++) {
-			if ((credit_line[credit_line_char_idx] == '\n') || (credit_line[credit_line_char_idx] == '\r') || (credit_line[credit_line_char_idx] == '\0')) {
-				credit_line[credit_line_char_idx] = ' ';
+		for (char_idx=0 ; char_idx<LINE_BUFFER_LENGTH ; char_idx++) {
+			if ((credit_line[char_idx] == '\n') || (credit_line[char_idx] == '\r') || (credit_line[char_idx] == '\0')) {
+				credit_line[char_idx] = ' ';
 			}
-			if (credit_line_char_idx > credit_line_length) {
-				credit_line[credit_line_char_idx] = ' ';
+			if (char_idx > credit_line_length) {
+				credit_line[char_idx] = ' ';
 			}
 		}
 		// Extract the line.
-		for (credit_line_char_idx=0 ; credit_line_char_idx<(BLENDER_CREDIT_LINE_LENGTH) ; credit_line_char_idx++) {
-			blender_line[(blender_subline_idx * BLENDER_CREDIT_LINE_LENGTH) + credit_line_char_idx] = credit_line[credit_line_char_idx];
+		for (char_idx=0 ; char_idx<BLENDER_CREDIT_LINE_LENGTH; char_idx++) {
+			blender_line[(blender_subline_idx * BLENDER_CREDIT_LINE_LENGTH) + char_idx] = credit_line[char_idx];
 		}
 		// Manage sub-line index.
 		blender_subline_idx++;
 		if (blender_subline_idx >= BLENDER_NUMBER_OF_LINE_PER_TEXT_OBJECT) {
 			// Write line in output file.
-			wprintf(L"blender_line = %ls\n", blender_line);
+			blender_line_idx++;
+			wprintf(L"Blender line %02d = %ls\n", blender_line_idx, blender_line);
 			fputws(blender_line, credits_out);
 			fputws(L"\n\n", credits_out);
 			// Reset index.
 			blender_subline_idx = 0;
+			// Reset blender line.
+			for (char_idx=0 ; char_idx<BLENDER_TEXT_OBJECT_MAX_LENGTH ; char_idx++) {
+				blender_line[char_idx] = '\0';
+			}
 		}
 	}
+	// Print last blender line.
+	blender_line_idx++;
+	wprintf(L"Blender line %02d = %ls\n", blender_line_idx, blender_line);
+	fputws(blender_line, credits_out);
+	fputws(L"\n\n", credits_out);
+	// Close files.
+	fclose(credits_in);
+	fclose(credits_out);
 errors:
 	// Return
 	return 0;
